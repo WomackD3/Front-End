2343d1f37d7f22893edd52433cf22ed5d009b427
Carlton James Jr <cjames222@Genayas-MacBook-Air.local>
2022-07-28 12:47:34 -0400

    Created register component, added countries json file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,11 @@
+import './App.scss'
+import navbar from './components/navbar'
 
 function App() {
   return (
     <div className="App">
-<h2>Wasssup Team of Greats</h2>
+      <h2>Wasssup Team of Greats</h2>
+      <navbar />
     </div>
   );
 }
