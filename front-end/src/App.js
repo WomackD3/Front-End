00d00cd3d3f34b334cd99708d0bcc18cdce1f899
Womack <D_wom3@yahoo.com>
2022-07-28 14:33:15 -0400

    updated Scss, fixed form input, shifted width

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,14 @@
 import './App.scss'
-import navbar from './components/navbar'
+import {RegisterForm} from './components/register.jsx'
 
 function App() {
   return (
     <div className="App">
       <h2>Wasssup Team of Greats</h2>
-      <navbar />
+
+      <div className="regForm">
+        <RegisterForm />
+        </div>
     </div>
   );
 }
