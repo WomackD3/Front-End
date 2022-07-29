fe873ed9e393275661c365e031be40d65939109e
Womack <D_wom3@yahoo.com>
2022-07-29 11:58:07 -0400

    Added NavBar and NavElements, need connection to routes in backend, updated app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,16 @@
-import './App2.scss'
-import {RegisterForm} from './components/register.jsx'
-
+import './App.scss'
+import { RegisterForm } from './components/register.jsx'
+import NavBar from './screens/NavBar';
+import { BrowserRouter as Router } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <h2>Wasssup Simpliciity</h2>
 
       <div className="regForm">
+        <Router>
+          {<NavBar />}
+          </Router>
         <RegisterForm />
         </div>
     </div>
