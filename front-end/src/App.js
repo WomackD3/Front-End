ef742add68b9459e8e84715b9cd704db343e6b46
Carlton James Jr <cjames222@Genayas-MacBook-Air.local>
2022-07-29 11:47:04 -0400

    Created Login form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,14 @@
 import './App.scss'
 import {RegisterForm} from './components/register.jsx'
+import { Login } from './components/login'
 
 function App() {
   return (
     <div className="App">
       <h2>Wasssup Simpliciity</h2>
 
-      <div className="regForm">
-        <RegisterForm />
+      <div>
+        <Login />
         </div>
     </div>
   );
