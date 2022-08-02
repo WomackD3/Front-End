c8ee1f49739ec280d5665507397ce5a63e82ef2a
Womack <D_wom3@yahoo.com>
2022-08-02 09:30:08 -0400

    made a few minor changes and cleared a few bugs

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,13 @@
 import './App.scss'
 import RegisterForm from './components/register.jsx'
 import Login from './components/login'
-import NavBar from './screens/NavBar';
+// import NavBar from './screens/NavBar';
 import { Routes, Route } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import SidebarDemo from './components/Sidebar.jsx';
 import { LightDark } from './components/LightDark';
 import Home from './screens/Home.jsx'
-import Posts from './components/Post.jsx'
+// import Posts from './components/Post.jsx'
 import Write from "./Write.jsx";
 
 
@@ -20,7 +20,7 @@ function App() {
       <h2>Wasssup  <span style={{ color: "red" }}>
         {user.isLoggedIn ? user.profile.name : "Guest"}
       </span></h2>
-      <NavBar />
+     
   
       
       <Routes>
@@ -28,9 +28,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/Register" element={<RegisterForm />} />
         <Route path="/Write" element={<Write />} />
+        <Route  path="/Sidebar" element={<SidebarDemo />} />
         <Route  path="/Sidebar" element={<LightDark />} />
         <Route  path="/" element={<SidebarDemo />} />
-        <Route  path="/Sidebar" element={<SidebarDemo />} />
         </Routes>
     </div>
   );
--- front-end/src/screens/Home.jsx
+++ front-end/src/screens/Home.jsx
@@ -1,13 +1,13 @@
 import '../components/post.css'
 import React from 'react'
 import Posts from '../components/Post.jsx'
-
+import NavBar from './NavBar'
 
 export default function Home() {
   
   return (
     <>
-    
+     <NavBar />
      
     <div className="posts">
       <Posts />
