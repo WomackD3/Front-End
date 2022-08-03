1e2060c78e8d745ceff2178b8a3e91e0ccf0354e
Womack <D_wom3@yahoo.com>
2022-08-03 09:32:09 -0400

    upstreamed and fixing up warnings

--- front-end/src/screens/Home.jsx
+++ front-end/src/screens/Home.jsx
@@ -1,13 +1,7 @@
-import '../components/post.css'
-//import './posts.css'
-//import './Posts2.jsx'
 import React from 'react'
-import MentionDemo from './Posts2'
-
 import NavBar from './NavBar'
 import CardDemo from './Posts3'
 import ToolBar from '../components/MakePost'
-import PhotoUpload from '../components/FileUpload'
 
 
 export default function Home() {
--- front-end/src/screens/Posts3.jsx
+++ front-end/src/screens/Posts3.jsx
@@ -17,13 +17,13 @@ export default function CardDemo() {
     const header = (
         <img alt="Card" src="images/usercard.png" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} />
     );
-    const footer = (
-        <span>
-            {/* <Button label="Save" icon="pi pi-check" />
-            <Button label="Cancel" icon="pi pi-times" className="p-button-secondary ml-2" /> */}
-            <InputText type="text" className="block mb-2" placeholder="Write a comment..." style={{width: '42em'}}/>
-        </span>
-    );
+    // const footer = (
+    //     <span>
+    //         {/* <Button label="Save" icon="pi pi-check" />
+    //         <Button label="Cancel" icon="pi pi-times" className="p-button-secondary ml-2" /> */}
+    //         {/* <InputText type="text" className="block mb-2" placeholder="Write a comment..." style={{width: '42em'}}/> */}
+    //     </span>
+    // );
 
     return (
         <div className="cardDemo">
