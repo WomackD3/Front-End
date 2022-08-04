1e12e08e43c24b3b805ccfb574fef93c1cf5d565
cjames222 <carltonjam@gmail.com>
2022-08-04 03:57:21 -0400

    Title, description, and photo can now be written to the backend

--- front-end/src/components/MakePost.jsx
+++ front-end/src/components/MakePost.jsx
@@ -1,52 +1,66 @@
 import React, { useState } from 'react';
 import { InputTextarea } from 'primereact/inputtextarea';
+import { InputText } from 'primereact/inputtext'
 import { Card } from 'primereact/card'
 import { FileUpload } from 'primereact/fileupload'
 import { Button } from 'primereact/button'
 import axios from 'axios'
 import { createPost } from '../service/posts'
 import api from '../service/apiConfig';
-import { ConnectedOverlayScrollHandler } from 'primereact/utils';
 
 export default function NewPost (props) {
+    const [title, setTitle] = useState({})
     const [value, setValue] = useState({});
-
-    const [image, setImage] = useState({
-        image: ''
-    })
-
-    const uploadDescription = async () => {
-        console.log(value)
-        let description = await api.post('http://localhost:3002/api/posts', value)
-        return description
-
-    }
-
-
+    const [image, setImage] = useState({})
 
     const uploadPhoto = async (props) => {
-       setImage(props.files[0].name)
-       console.log(image) 
-       let userImage =  await api.post('http://localhost:3002/api/posts', image)
-       console.log(userImage)
-       return userImage
+        setImage(props.files[0].name)
+        console.log(image)
+        return image
     }
 
+    const post = async () => {
+        console.log(image)
+        let newObj = {
+            title: title,
+            image: image,
+            description: value
+        }
+       
+
+       let userPost =  await api.post('http://localhost:3002/api/posts', newObj)
+       console.log(userPost)
+       return userPost
+    }
+     
+    
+
+
+
+    
+
     // const uploadPhoto = async (props) => {
-    //     setImage(props.files[0].name)
-    //     console.log(setImage) 
-    //     let userImage =  image
-    //     console.log(userImage)
-    //  }
+    // let newObj = {
+    //     image: value
+    // }
+
+    // let userImage =  await createPost(newObj)
+    // console.log(userImage)
+    // return userImage
+    // }
 
     return (
         <div>
             <div className="card">
                 <Card style={{width: '50%'}}>
-                    <InputTextarea style={{width: '100%'}} placeholder='Write something...' name='description' value={value} onChange={(e) => setValue({...value,[e.target.name]: e.target.value})} rows={5} cols={30} />
-                    <FileUpload name="image" url='http://localhost:3002/api/posts' customUpload uploadHandler={uploadPhoto} auto />
+                    <InputText type="text" style={{width: '100%'}}className="p-inputtext-lg block"  placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
                     <br/>
-                    <Button onClick={uploadDescription}>Create Post</Button>
+                    <InputTextarea style={{width: '100%'}} placeholder='Write something...' name='description' value={value} onChange={(e) => setValue(e.target.value)} rows={5} cols={30} />
+                    <br/>
+                    <br/>
+                    <FileUpload name="image" url='http://localhost:3002/api/posts' customUpload uploadHandler={uploadPhoto} auto/>
+                    <br/>
+                    <Button onClick={post}>Create Post</Button>
                 </Card>
             </div>
         </div>
